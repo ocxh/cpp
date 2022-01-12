7732f12089700bf5fa0eec1819df1384057164c4
한결같음 <83993373+ocxh@users.noreply.github.com>
2022-01-13 00:30:28 +0900

    Add Func: Replace

--- SW캠프 with C/SWcamp_project/SWcamp_project.cpp
+++ SW캠프 with C/SWcamp_project/SWcamp_project.cpp
@@ -9,7 +9,9 @@ typedef struct Node
 	struct Node* rp;
 }Node;
 
-void Insert(Node** start, char filename[]); //Linked List에 노드 추가
+void Insert(Node** start, char filename[]); //노드 추가
+void Delete(Node** start, char filename[]); //노드 삭제
+void Replace(Node** start, char word[], char Rword[]); //특정 단어 수정/삭제
 bool ckWord(char filename[], char word[]); //같은 단어 있는지 체크
 void prtnode(Node* start); //LinkedList 출력
 void free_node(Node* start); //LinkedList free()
@@ -18,6 +20,7 @@ int main()
 {
 	char filename[20];
 	char word[20];
+	char Rword[20];
 	Node* start_p = NULL;
 
 	printf("단어: ");
@@ -34,6 +37,22 @@ int main()
 		}
 	}
 	prtnode(start_p);
+	while (1)
+	{
+		printf("제외할 파일명(0=종료): ");
+		scanf("%s", filename);
+		if (!strcmp(filename, "0")) break;
+
+		Delete(&start_p, filename);
+	}
+	prtnode(start_p);
+
+	printf("수정할 단어(엔터시 삭제): ");
+	getc(stdin); //\n을 버퍼에서 빼오기(해당 코드 누락 시 gets_s 씹힘 
+	gets_s(Rword);
+	printf("%s\n", Rword);
+	Replace(&start_p, word, Rword);
+
 	free_node(start_p);
 	return 0;
 }
@@ -60,6 +79,66 @@ void Insert(Node** start, char filename[])
 		}
 		new_p->lp = pre_p;
 		pre_p->rp = new_p;
+	}
+}
+
+void Delete(Node** start, char filename[])
+{
+	Node* now = *start;
+	Node* pre_p = NULL;
+	Node* next_p = NULL;
+	while (now != NULL)
+	{
+		pre_p = now->lp;
+		next_p = now->rp;
+		if (!strcmp(now->fname, filename))
+		{
+			if (pre_p == NULL) //해당 파일이름의 노드가 첫번째 노드라면
+			{
+				free(now);
+				*start = next_p;
+			}
+			else if (next_p == NULL) //해당 파일이름의 노드가 마지막 노드라면
+			{
+				free(now);
+				pre_p->rp = NULL;
+			}
+			else
+			{
+				pre_p->rp = next_p;
+				next_p->lp = pre_p;
+				free(now);
+			}
+			printf("[삭제완료] 파일명: %s\n", filename);
+			return;
+		}
+		now = next_p;
+	}
+	printf("[Error]해당 이름의 파일이 존재하지 않습니다\n");
+}
+
+void Replace(Node** start, char word[], char Rword[])
+{
+	Node* now = *start;
+	char nword[20];
+	int slen = 0;
+	while (now != NULL)
+	{
+		FILE* fp = fopen(now->fname, "r+");
+		while (!feof(fp))
+		{
+			fscanf(fp, "%s", nword);
+			nword[19] = NULL;
+			if (!strcmp(word, nword))
+			{
+				fseek(fp, -1*strlen(nword), SEEK_CUR);
+				fwrite(Rword, strlen(Rword), 1, fp);
+				printf("변경성공\n");
+				break;
+			}
+		}
+		fclose(fp);
+		now = now->rp;
 	}
 }
 
