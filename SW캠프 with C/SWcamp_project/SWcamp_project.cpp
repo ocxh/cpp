78d7368205748f8f71d303df3566c83e2276bc5e
한결같음 <83993373+ocxh@users.noreply.github.com>
2022-01-12 14:56:59 +0900

    Add Func(Insert)

--- SW캠프 with C/SWcamp_project/SWcamp_project.cpp
+++ SW캠프 with C/SWcamp_project/SWcamp_project.cpp
@@ -9,6 +9,7 @@ typedef struct Node
 	struct Node* rp;
 }Node;
 
+void Insert(Node** start, char filename[]); //Linked List에 노드 추가
 bool ckWord(char filename[], char word[]); //같은 단어 있는지 체크
 void prtnode(Node* start); //LinkedList 출력
 void free_node(Node* start); //LinkedList free()
@@ -17,41 +18,49 @@ int main()
 {
 	char filename[20];
 	char word[20];
-	Node* p = NULL;
-	Node* pre_p = NULL;
-	//Node* next_p = NULL;
 	Node* start_p = NULL;
 
+	printf("단어: ");
+	scanf("%s", word);
 	while (1)
 	{
 		printf("파일 이름(0=종료): ");
 		scanf("%s", filename);
 		if(!strcmp(filename,"0")) break;
 
-		printf("단어: ");
-		scanf("%s", word);
 		if (ckWord(filename, word))
 		{
-			p = (Node*)malloc(sizeof(Node));
-			strcpy(p->fname, filename);
-			if (start_p == NULL)
-			{
-				p->lp = NULL;
-				p->rp = NULL;
-				start_p = p;
-			}
-			else
-			{
-				pre_p->rp = p;
-				p->lp = pre_p;
-				p->rp = NULL;
-			}
-			pre_p = p;
+			Insert(&start_p, filename);
 		}
 	}
 	prtnode(start_p);
 	free_node(start_p);
 	return 0;
+}
+
+void Insert(Node** start, char filename[])
+{
+	Node* now = *start;
+	Node* new_p = (Node*)malloc(sizeof(Node));
+	Node* pre_p = NULL;
+
+	strcpy(new_p->fname, filename);
+	new_p->lp = NULL;
+	new_p->rp = NULL;
+
+	if (*start == NULL)
+	{
+		*start = new_p;
+	}
+	else {
+		while (now != NULL)
+		{
+			pre_p = now;
+			now = now->rp;
+		}
+		new_p->lp = pre_p;
+		pre_p->rp = new_p;
+	}
 }
 
 bool ckWord(char *filename, char *word)
