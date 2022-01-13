08377edc5db01c973bfbb8cb93c41a6e29edcc09
한결같음 <83993373+ocxh@users.noreply.github.com>
2022-01-14 01:04:28 +0900

    Replace Func: string

--- SW캠프 with C/SWcamp_project/SWcamp_project.cpp
+++ SW캠프 with C/SWcamp_project/SWcamp_project.cpp
@@ -9,6 +9,10 @@ typedef struct Node
 	struct Node* rp;
 }Node;
 
+//문자열 처리 함수
+void SearchW(char* st, char word[], int* index1, int* index2);
+char* Nstring(char* st, int index1, int index2, char Nword[]);
+//노드 관련 함수
 void Insert(Node** start, char filename[]); //노드 추가
 void Delete(Node** start, char filename[]); //노드 삭제
 void Replace(Node** start, char word[], char Rword[]); //특정 단어 수정/삭제
@@ -23,10 +27,12 @@ int main()
 	char Rword[20];
 	Node* start_p = NULL;
 
+	//검색할 단어 입력 받기
 	printf("단어: ");
 	scanf("%s", word);
 	while (1)
 	{
+		//해당 단어 검색할 파일명 입력 받기
 		printf("파일 이름(0=종료): ");
 		scanf("%s", filename);
 		if(!strcmp(filename,"0")) break;
@@ -39,6 +45,7 @@ int main()
 	prtnode(start_p);
 	while (1)
 	{
+		//만들어진 Linked List에서 제외할 파일 입력
 		printf("제외할 파일명(0=종료): ");
 		scanf("%s", filename);
 		if (!strcmp(filename, "0")) break;
@@ -47,7 +54,7 @@ int main()
 	}
 	prtnode(start_p);
 
-	printf("수정할 단어(엔터시 삭제): ");
+	printf("수정할 단어: ");
 	getc(stdin); //\n을 버퍼에서 빼오기(해당 코드 누락 시 gets_s 씹힘 
 	gets_s(Rword);
 	printf("%s\n", Rword);
@@ -55,6 +62,44 @@ int main()
 
 	free_node(start_p);
 	return 0;
+}
+
+void SearchW(char* st, char word[], int* index1, int* index2)
+{
+	char s[100];
+	char check_s[20] = "";
+	char w[20];
+
+	strcpy(s, st);
+	strcpy(w, word);
+
+	for (int i = 0; i < strlen(s); i++)
+	{
+		memcpy(check_s, &s[i], strlen(word));
+		if (!strcmp(check_s, word))
+		{
+			*index1 = i + 1;
+			*index2 = i + strlen(word);
+		}
+
+	}
+}
+
+char* Nstring(char* st, int index1, int index2, char Nword[])
+{
+	char s1[100] = "";
+	char s2[100] = "";
+	char line[100];
+	char new_line[100];
+
+	strcpy(line, st); //기존 문자열 가져오기
+	memcpy(s1, &line[0], index1 - 1); //s1에 line[0]부터 단어의 맨 앞 인덱스까지 복사
+	memcpy(s2, &line[index2], strlen(line) - index2); //s2에 line[인덱스2]부터 기존 문자열-인덱스2 까지
+	strcpy(new_line, s1); //새로운 문자열에 우선 s1넣기
+	strcat(new_line, Nword); //새로운 문자열(s1)에 새로운 단어 추가
+	strcat(new_line, s2); //새로운 문자열(s1+Nword)에 s2 넣기
+	//new_line[99] = NULL; //마지막은 NULL
+	return new_line; //return
 }
 
 void Insert(Node** start, char filename[])
@@ -117,28 +162,53 @@ void Delete(Node** start, char filename[])
 	printf("[Error]해당 이름의 파일이 존재하지 않습니다\n");
 }
 
-void Replace(Node** start, char word[], char Rword[])
+void Replace(Node** start, char word[], char Rword[]) //기존 단어word, 교체 단어 Rword
 {
-	Node* now = *start;
-	char nword[20];
-	int slen = 0;
+	Node* now = *start; //시작노드
 	while (now != NULL)
 	{
+		int index1, index2;
+		char line[256];
+		char new_line[256];
+		int alls = 0; //누적 문자열 길이
+		char* exist = NULL;
+		
 		FILE* fp = fopen(now->fname, "r+");
 		while (!feof(fp))
 		{
-			fscanf(fp, "%s", nword);
-			nword[19] = NULL;
-			if (!strcmp(word, nword))
+			fgets(line, sizeof(line), fp); //파일에서 한 줄 읽어오기
+
+			if (strstr(line, word) != NULL) //단어를 찾으면
 			{
-				fseek(fp, -1*strlen(nword), SEEK_CUR);
-				fwrite(Rword, strlen(Rword), 1, fp);
-				printf("변경성공\n");
+				printf("찾음: %s\n", line);
+				//새로운 문자열 생성
+				SearchW(line, word, &index1, &index2);
+				strcpy(new_line, Nstring(line, index1, index2, Rword));
+				printf("새로 만들어진 line: %s", new_line);
+				//파일 포인터를 해당 라인의 처음으로 옮김
+				if (alls == 0) fseek(fp, alls, SEEK_SET); //첫줄은 파일포인터 0부터
+				else fseek(fp, alls+1, SEEK_SET); //그 외에는 누적 문자열길이+1 만큼 이동
+
+				//새로운 문자열 쓰기
+				fputs(new_line, fp);
 				break;
+			//fscanf(fp, "%s", nword);
+			//nword[19] = NULL;
+			//if (!strcmp(word, nword))
+			//{
+			//	fseek(fp, -1*strlen(nword), SEEK_CUR);
+			//	fwrite(Rword, strlen(Rword), 1, fp);
+			//	for (int i = strlen(Rword); i < strlen(word)+1; i++)
+			//	{
+			//		fwrite("\0", 1, 1, fp);
+			//	}
+			//	printf("변경성공\n");
+			//	break;
 			}
+			alls += strlen(line)-1; //현재까지 읽은 줄 누적 길이
 		}
 		fclose(fp);
-		now = now->rp;
+		now = now->rp; //다음 파일로
 	}
 }
 
