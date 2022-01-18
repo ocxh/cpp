13502e7e66791a5ae84443ad0b30d6f84880735b
한결같음 <83993373+ocxh@users.noreply.github.com>
2022-01-18 18:36:51 +0900

    열혈자료구조는 std_algorithm 레파지토리에

--- 열혈 자료구조/DataStructure/Fibonacci.c
+++
@@ -1,6 +0,0 @@
-int Fibonacci(int cnt)
-{
-	if (cnt == 1) return 0;
-	else if (cnt == 2) return 1;
-	else return Fibonacci(cnt - 1) + Fibonacci(cnt - 2);
-}
--- 열혈 자료구조/DataStructure/Hanoi.c
+++
@@ -1,6 +0,0 @@
-void Hanoi(int num, int x, int y)
-{
-	if (num > 1) Hanoi(num - 1, x, 6 - x - y);
-	printf("%d %d\n", x, y);
-	if (num > 1) Hanoi(num - 1, 6 - x - y, y);
-}
